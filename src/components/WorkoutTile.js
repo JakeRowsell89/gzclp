3ca40ca6761ebaf846696d10aa4480ef3cc5d13c
Jake Rowsell <jakerowsell@bgl-mac-009556.bgl.financial.local>
2019-04-13 13:02:09 +0100

    🎨Colour code successful and failed workouts

--- src/components/WorkoutTile.js
+++ src/components/WorkoutTile.js
@@ -5,10 +5,10 @@ import './WorkoutTile.css';
 class Footer extends Component {
   render() {
     return (
-      <div className="Workout-tile">
+      <div className={"Workout-tile " + (this.props.workout.completed ? 'complete' : 'incomplete')}>
         <div className="date">{this.props.workout.date ? this.props.workout.date : 'Next'}</div>
         {this.props.workout.exercises.map((exercise, i) => (
-          <div key={i} className="tile-row">
+          <div key={i} className={"tile-row " + (exercise.completed ? 'complete' : 'incomplete')}>
             <div className="tier">T{exercise.tier}</div>
             <div className="exercise">{exercise.name}</div>
             <div className="weight">{exercise.weight.amount} {exercise.weight.unit}</div>
