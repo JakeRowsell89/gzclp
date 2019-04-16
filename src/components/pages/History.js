9173dcf52d76dce43f3d4f60fd31e3d621286005
Jake Rowsell <jakerowsell@bgl-mac-009556.bgl.financial.local>
2019-04-16 12:37:22 +0100

    ✨History page

--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import WorkoutTile from '../WorkoutTile'
+import workouts from '../../fixtures/workouts.json'
 
+import './History.css'
 class History extends Component {
   render() {
     return (
-      <div className="History-page">History page</div>
+      <div className="History-page">
+        {
+          workouts.map((workout, i) => <WorkoutTile key={i} workout={workout} />
+          )
+        }
+      </div>
     )
   }
 }
