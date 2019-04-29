500eb4242c1a7219513bc25f508b29d5ff391676
Jake Rowsell <jakerowsell@bgl-mac-009556.bgl.financial.local>
2019-04-29 08:18:43 +0100

    ♻️WorkoutTile as stateless component

--- src/components/WorkoutTile.js
+++ src/components/WorkoutTile.js
@@ -1,65 +1,63 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import './WorkoutTile.css'
 
-class WorkoutTile extends Component {
-  render() {
-    const day = this.props.workout.day
+const WorkoutTile = props => {
+  const day = props.workout.day
 
-    return (
-      <div
-        className={`Workout-tile ${
-          this.props.workout.completed ? 'complete' : 'incomplete'
-        }`}
-      >
-        <div className="tile-row">
-          <div className="day">
-            Day
-            {(!this.props.workout.completed && (
-              <select defaultValue={day} onChange={this.props.changeHandler}>
-                {[1, 2, 3, 4].map((n, i) => {
-                  return (
-                    <option key={i} value={n}>
-                      {n}
-                    </option>
-                  )
-                })}
-              </select>
-            )) ||
-              day}
-          </div>
-          <div className="date">
-            {this.props.workout.date ? (
-              this.props.workout.date
-            ) : (
-              <Link to={{ pathname: '/workout', workout: this.props.workout }}>
-                <span role="img" aria-label="liftoff">
-                  🚀
-                </span>
-              </Link>
-            )}
-          </div>
+  return (
+    <div
+      className={`Workout-tile ${
+        props.workout.completed ? 'complete' : 'incomplete'
+      }`}
+    >
+      <div className="tile-row">
+        <div className="day">
+          Day
+          {(!props.workout.completed && (
+            <select defaultValue={day} onChange={props.changeHandler}>
+              {[1, 2, 3, 4].map((n, i) => {
+                return (
+                  <option key={i} value={n}>
+                    {n}
+                  </option>
+                )
+              })}
+            </select>
+          )) ||
+            day}
         </div>
-        {this.props.workout.exercises.map((exercise, i) => (
-          <div
-            key={i}
-            className={`tile-row ${
-              exercise.completed ? 'complete' : 'incomplete'
-            }`}
-          >
-            <div className="tier">T{exercise.tier}</div>
-            <div className="exercise">{exercise.name}</div>
-            <div className="weight">
-              {exercise.weight.amount} {exercise.weight.unit}
-            </div>
-            <div className="format">{exercise.format}</div>
-          </div>
-        ))}
+        <div className="date">
+          {props.workout.date ? (
+            props.workout.date
+          ) : (
+            <Link to={{ pathname: '/workout', workout: props.workout }}>
+              <span role="img" aria-label="liftoff">
+                🚀
+              </span>
+            </Link>
+          )}
+        </div>
       </div>
-    )
-  }
+      {props.workout.exercises.map((exercise, i) => (
+        <div
+          key={i}
+          className={`tile-row ${
+            exercise.completed ? 'complete' : 'incomplete'
+          }`}
+        >
+          <div className="tier">T{exercise.tier}</div>
+          <div className="exercise">{exercise.name}</div>
+          <div className="weight">
+            {exercise.weight.amount} {exercise.weight.unit}
+          </div>
+          <div className="format">{exercise.format}</div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 WorkoutTile.propTypes = {
@@ -68,6 +66,7 @@ WorkoutTile.propTypes = {
   'workout.day': PropTypes.number,
   'workout.date': PropTypes.instanceOf(Date),
   'workout.exercises': PropTypes.array,
+  changeHandler: PropTypes.func,
 }
 
 export default WorkoutTile
